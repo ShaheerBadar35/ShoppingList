4de9889d4184dd90212bc675f15970d831001a7c
ShaheerBadar35 <shaheerbadar35@gmail.com>
2024-08-08 23:56:04 +0500

    third commit

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,12 +11,36 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => NewItem()),
+    );
+    if (newItem == null) {
+      return;
+    }
+    _groceryItems.add(newItem);
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text("No item added yet.!"),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(_groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: _groceryItems[index].category.color,
+          ),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries!'),
@@ -26,17 +51,7 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(groceryItems[index].quantity.toString()),
-        ),
-      ),
+      body: content,
     );
   }
 }
